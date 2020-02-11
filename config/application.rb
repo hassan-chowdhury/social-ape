373cb2f679f5308ef062e9a01f42474fb9ffa60e
hassan chowdhury <hassan.chowdhury@sage.com>
2020-02-11 12:34:33 +0000

    user authentication/authorization

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,10 @@ module SocialapeApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    
+    # config for jwt
+    config.autoload_paths << Rails.root.join('lib')
+    
     config.api_only = true
   end
 end
