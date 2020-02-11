a4045e974b53ed9da5d8bf6f072bf2643b3a307c
hassan chowdhury <hassan.chowdhury@sage.com>
2020-02-11 14:43:43 +0000

    user registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  post 'authenticate', to: 'authentication#authenticate'
+  # post 'authenticate', to: 'authentication#authenticate'
+  post 'auth/register', to: 'users#register'
+  post 'auth/login', to: 'users#login'
 
 end
--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -4,14 +4,18 @@ class JsonWebToken
 
     class << self
         def encode(payload, exp = 2.hours.from_now)
+            # set token expiration time
             payload[:exp] = exp.to_i
+            # encode user data (payload) with the secret key
             JWT.encode(payload, Rails.application.secrets.secret_key_base)
         end
 
         def decode(token)
+            # decode the token to get the user data (payload)
             body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
             HashWithIndifferentAccess.new body
         rescue
+            # TODO add error handling code
             nil
         end
     end
